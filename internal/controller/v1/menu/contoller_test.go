c4da7b37fd684470b50ba283c23a85b720b6e6b0
v.koshelnyk <valentyna.koshelnyk@deliveryhero.com>
2024-05-13 09:22:49 +0200

    Add menu test

--- internal/controller/v1/menu/contoller_test.go
+++ internal/controller/v1/menu/contoller_test.go
@@ -3,9 +3,6 @@ package menu
 import (
 	"encoding/json"
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-	"github.com/pkg/errors"
-	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/valentyna-koshelnyk/panda-eats-prototype-api/internal/domain/entity"
@@ -15,47 +12,56 @@ import (
 	"testing"
 )
 
-// mocking data : Mockery
-// test compare responses
-
-//go:generate mockery
-
-// we need to set up a new server
-// and to compare response
-// mocking the client - not yet
-// when this client is called with these methods and these arguments then return this response
-// http (server and route, and api client
-
 func TestGetMenuByRestaurant(t *testing.T) {
-	t.Run("should return menu", func(t *testing.T) {
-		r := chi.NewRouter()
-		server := &http.Server{
-			Addr:    ":" + "3000",
-			Handler: r,
-		}
-		r.Use(middleware.RequestID)
-		log.Info("Starting server on port : 3000")
-		if err := server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
-			log.Fatalf("HTTP server error: %v", err)
-		}
+	r := chi.NewRouter()
 
-		mockService := new(mocks.MenuService)
-		expectedMenu := []entity.Menu{{RestaurantID: 1, Category: "Extra Large Pizza", Name: "Extra Large 5 Cheese Slice", Description: "Slice.", Price: "3.99 USD"}}
-		mockService.On("GetMenuByRestaurant", mock.Anything, mock.Anything).Return(expectedMenu, nil)
-		controller := NewController(mockService)
-		r.Route("/api/v1/restaurants/{restaurant_id}/items", func(r chi.Router) {
-			r.Get("/", controller.GetMenuByRestaurant)
-		})
+	mockService := new(mocks.MenuService)
+	controller := Controller{
+		service: mockService,
+	}
 
-		req, _ := http.NewRequest(http.MethodGet, "/api/v1/restaurants/1/items", nil)
+	r.Get("/api/v1/restaurants/{restaurant_id}/items", controller.GetMenuByRestaurant)
+
+	t.Run("should return 200 OK response", func(t *testing.T) {
+		menus := []entity.Menu{}
+		mockService.On("GetMenu", mock.AnythingOfType("int64")).Return(&menus, nil)
+		req := httptest.NewRequest(http.MethodGet, "/api/v1/restaurants/1/items", nil)
 		rec := httptest.NewRecorder()
 		r.ServeHTTP(rec, req)
-
-		assert.NotNil(t, rec.Code)
 		assert.Equal(t, http.StatusOK, rec.Code)
-		expectedJSON, _ := json.Marshal(expectedMenu)
-		assert.JSONEq(t, string(expectedJSON), rec.Body.String(), "Expected menu to match")
+	})
 
-		mockService.AssertExpectations(t)
+	t.Run("should return menu of the restaurant", func(t *testing.T) {
+		menus := []entity.Menu{
+			{
+				RestaurantID: 1,
+				Category:     "Extra Large Pizza",
+				Name:         "Extra Large Supreme Slice",
+				Description:  "Slice.",
+				Price:        "3.99 USD",
+			},
+		}
+		mockService.On("GetMenu", mock.AnythingOfType("int64")).Return(&menus, nil)
+		req := httptest.NewRequest(http.MethodGet, "/api/v1/restaurants/1/items", nil)
+		rec := httptest.NewRecorder()
+
+		r.ServeHTTP(rec, req)
+
+		respBody := rec.Body.Bytes()
+		t.Logf("Response Body: %s", respBody)
+
+		var result []entity.Menu
+		err := json.Unmarshal(respBody, &result)
+		if err != nil {
+			t.Errorf("Failed to unmarshal response body: %v", err)
+		}
+
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, 1, len(result))
+		if len(result) > 0 {
+			assert.Equal(t, int64(1), result[0].RestaurantID)
+			assert.Equal(t, "Extra Large Pizza", result[0].Category)
+			assert.Equal(t, "Slice.", result[0].Description)
+		}
 	})
 }
