be8dc84cc1a3b9ed915f6a42b206872f29e5fe49
v.koshelnyk <valentyna.koshelnyk@deliveryhero.com>
2024-05-14 10:22:53 +0200

    Fix tests and menu controller

--- internal/controller/v1/menu/contoller_test.go
+++ internal/controller/v1/menu/contoller_test.go
@@ -11,64 +11,6 @@ import (
 	"testing"
 )
 
-// mocking data : Mockery
-// test compare responses
-
-//go:generate mockery
-
-// we need to set up a new server
-// and to compare response
-// mocking the client - not yet
-// when this client is called with these methods and these arguments then return this response
-// http (server and route, and api client
-
-//func TestGetMenuByRestaurant(t *testing.T) {
-//	r := chi.NewRouter()
-//
-//	mockService := new(mocks.MenuService)
-//	controller := Controller{
-//		service: mockService,
-//	}
-//
-//	r.Get("/api/v1/restaurants/{restaurant_id}/items", controller.GetMenuByRestaurant)
-//
-//	t.Run("should return 200 OK response", func(t *testing.T) {
-//		menus := []*entity.Menu{}
-//		mockService.On("GetMenu", mock.AnythingOfType("int64")).Return(menus, nil)
-//		req := httptest.NewRequest(http.MethodGet, "/api/v1/restaurants/1/items", nil)
-//		rec := httptest.NewRecorder()
-//		r.ServeHTTP(rec, req)
-//		assert.Equal(t, http.StatusOK, rec.Code)
-//
-//	})
-//
-//	t.Run("should return menu of the restaurant", func(t *testing.T) {
-//		menus := []*entity.Menu{
-//			{
-//				RestaurantID: 1,
-//				Category:     "Extra Large Pizza",
-//				Name:         "Extra Large Supreme Slice",
-//				Description:  "Slice.",
-//				Price:        "3.99 USD",
-//			},
-//		}
-//		mockService.On("GetMenu", mock.AnythingOfType("int64")).Return(menus, nil)
-//
-//		req := httptest.NewRequest(http.MethodGet, "/api/v1/restaurants/1/items", nil)
-//		rec := httptest.NewRecorder()
-//
-//		r.ServeHTTP(rec, req)
-//
-//		var result []entity.Menu
-//		json.Unmarshal(rec.Body.Bytes(), &result)
-//
-//		assert.Equal(t, 1, len(result))
-//		assert.Equal(t, "1", result[0].RestaurantID)
-//		assert.Equal(t, "Extra Large Pizza", result[0].Category)
-//		assert.Equal(t, "Slice.", result[0].Description)
-//	})
-//}
-
 func TestGetMenuByRestaurant(t *testing.T) {
 
 	t.Run("should correctly retrieve and return menu details", func(t *testing.T) {
@@ -84,7 +26,7 @@ func TestGetMenuByRestaurant(t *testing.T) {
 			{RestaurantID: 1, Category: "Extra Large Pizza", Name: "Extra Large Supreme Slice", Description: "Slice.", Price: "3.99 USD"},
 		}
 
-		mockService.On("GetMenu", 1).Return(&menus, nil)
+		mockService.On("GetMenu", int64(1)).Return(&menus, nil)
 
 		req := httptest.NewRequest(http.MethodGet, "/api/v1/restaurants/1/items", nil)
 		rec := httptest.NewRecorder()
@@ -97,11 +39,9 @@ func TestGetMenuByRestaurant(t *testing.T) {
 		assert.Equal(t, http.StatusOK, rec.Code)
 		assert.NoError(t, err)
 		assert.Equal(t, 1, len(result))
-		if len(result) > 0 {
-			assert.Equal(t, int64(1), result[0].RestaurantID)
-			assert.Equal(t, "Extra Large Pizza", result[0].Category)
-			assert.Equal(t, "Slice.", result[0].Description)
-		}
+		assert.Equal(t, int64(1), result[0].RestaurantID)
+		assert.Equal(t, "Extra Large Pizza", result[0].Category)
+		assert.Equal(t, "Slice.", result[0].Description)
 	})
 
 	t.Run("should return empty string if menu not found", func(t *testing.T) {
@@ -120,5 +60,6 @@ func TestGetMenuByRestaurant(t *testing.T) {
 		respBody := rec.Body.Bytes()
 		assert.Equal(t, "", string(respBody))
 		assert.Equal(t, http.StatusNotFound, rec.Code)
+
 	})
 }
--- internal/controller/v1/menu/controller.go
+++ internal/controller/v1/menu/controller.go
@@ -5,6 +5,7 @@ import (
 	"github.com/go-chi/chi/v5"
 	log "github.com/sirupsen/logrus"
 	"github.com/valentyna-koshelnyk/panda-eats-prototype-api/internal/domain/service"
+	"reflect"
 
 	"net/http"
 	"strconv"
@@ -33,27 +34,27 @@ func NewController(service service.MenuService) *Controller {
 func (c *Controller) GetMenuByRestaurant(w http.ResponseWriter, r *http.Request) {
 	idStr := chi.URLParam(r, "restaurant_id")
 	id, err := strconv.ParseInt(idStr, 10, 64)
+	w.Header().Set("Content-Type", "application/json")
 	if err != nil {
 		http.Error(w, "Invalid restaurant ID", http.StatusBadRequest)
 		return
 	}
-	m, err := c.service.GetMenu(id)
-	w.Header().Set("Content-Type", "application/json")
-	// if service returns null since menu doesn't exist in db
-	if m == nil {
+	menuList, err := c.service.GetMenu(id)
+	if err != nil {
+		log.Errorf("Error while writing response: %s", err)
+		http.Error(w, "Error retrieving menu", http.StatusInternalServerError)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	} else if menuList == nil || reflect.ValueOf(menuList).IsZero() {
 		w.WriteHeader(http.StatusNotFound)
 		_, err = w.Write([]byte(""))
-	} else if err != nil {
-		log.Errorf("Error while writing response: %s", err)
 		return
-	} else {
-		w.WriteHeader(http.StatusOK)
-		menuJSON, _ := json.Marshal(m)
-		_, err = w.Write([]byte(menuJSON))
 	}
+	w.WriteHeader(http.StatusOK)
+	menuJSON, _ := json.Marshal(menuList)
+	_, err = w.Write([]byte(menuJSON))
 	if err != nil {
 		log.WithError(err).Error("Failed to encode restaurants into JSON")
 		return
 	}
-
 }
