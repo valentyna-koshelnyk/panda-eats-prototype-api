14f0ea52f339615d6e20dc5b0b51ee75ce3af6b7
v.koshelnyk <valentyna.koshelnyk@deliveryhero.com>
2024-05-21 18:52:04 +0200

    #14: Comment errors used for validation

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -7,10 +7,14 @@ import (
 )
 
 var (
-	ErrEmptyUserField  = errors.New("username, password and email can't be empty")
+	// ErrEmptyUserField checks if all necessary fields of user struct are filled out.
+	ErrEmptyUserField = errors.New("username, password and email can't be empty")
+	// ErrFieldWithSpaces checks if no spaces were used since spaces can introduce ambiguity and potential security risks.
 	ErrFieldWithSpaces = errors.New("username and password can't have spaces")
-	ErrShortPassword   = errors.New("password shorter than 8 characters")
-	ErrInvalidEmail    = errors.New("invalid email address")
+	// ErrShortPassword checks the length of password to avoid potential security risks.
+	ErrShortPassword = errors.New("password shorter than 8 characters")
+	//ErrInvalidEmail checks if email consists of valid characters.
+	ErrInvalidEmail = errors.New("invalid email address")
 )
 
 // ErrorResponse has only error field which holds the message to be returned in response
