e4300c0fcda78215188ab92edd03c65fa9bdd7a1
v.koshelnyk <valentyna.koshelnyk@deliveryhero.com>
2024-05-27 10:22:19 +0200

    #14 docs: add comments to user service methods

--- internal/domain/service/user.go
+++ internal/domain/service/user.go
@@ -5,6 +5,7 @@ import (
 	"github.com/valentyna-koshelnyk/panda-eats-prototype-api/internal/domain/repository"
 )
 
+// UserService defines an API for user service to be used by presentation layer
 type UserService interface {
 	CreateUser(user entity.User) (entity.User, error)
 	GetUser(id int64, username string, email string) (*entity.User, error)
@@ -12,10 +13,12 @@ type UserService interface {
 	DeleteUser(id int64) error
 }
 
+// userService struct as a business layer between controller and repository
 type userService struct {
 	repository repository.UserRepository
 }
 
+// NewUserService a constructor for user service
 func NewUserService(repository repository.UserRepository) UserService {
 	return &userService{repository: repository}
 }
@@ -28,6 +31,7 @@ func (s *userService) CreateUser(u entity.User) (entity.User, error) {
 	return u, nil
 }
 
+// UpdateUser takes user information, updates his information and returns user struct
 func (s *userService) UpdateUser(user entity.User) (entity.User, error) {
 	err := s.repository.UpdateUser(&user)
 	if err != nil {
@@ -36,6 +40,7 @@ func (s *userService) UpdateUser(user entity.User) (entity.User, error) {
 	return user, nil
 }
 
+// DeleteUser takes user id and deletes users record
 func (s *userService) DeleteUser(id int64) error {
 	err := s.repository.DeleteUser(id)
 	if err != nil {
@@ -44,6 +49,7 @@ func (s *userService) DeleteUser(id int64) error {
 	return nil
 }
 
+// GetUser retrieves the user based on his id, username and/or email
 func (s *userService) GetUser(id int64, username string, email string) (*entity.User, error) {
 	user, err := s.repository.GetUser(id, username, email)
 	if err != nil {
