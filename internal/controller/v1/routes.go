cd4f7222d6a56a0f192f6d32a9b1bb052a8afb43
v.koshelnyk <valentyna.koshelnyk@deliveryhero.com>
2024-06-18 09:58:21 +0200

    [] refactor: initialise all controllers in main

--- internal/controller/v1/routes.go
+++ internal/controller/v1/routes.go
@@ -4,18 +4,29 @@ import (
 	"github.com/go-chi/chi/v5"
 	httpSwagger "github.com/swaggo/http-swagger"
 	"github.com/valentyna-koshelnyk/panda-eats-prototype-api/internal/controller/v1/cart"
-
+	"github.com/valentyna-koshelnyk/panda-eats-prototype-api/internal/controller/v1/menu"
+	"github.com/valentyna-koshelnyk/panda-eats-prototype-api/internal/controller/v1/order"
 	"github.com/valentyna-koshelnyk/panda-eats-prototype-api/internal/controller/v1/restaurant"
 	"github.com/valentyna-koshelnyk/panda-eats-prototype-api/internal/controller/v1/user"
 )
 
+type HTTPController struct {
+	Menu       menu.MenuController
+	Restaurant restaurant.RestaurantController
+	Cart       cart.CartController
+	Order      order.OrderController
+	User       user.UserController
+}
+
 // Routes mounts routes of v1 API
-func Routes() chi.Router {
+func Routes(c *HTTPController) chi.Router {
 	r := chi.NewRouter()
 
-	r.Mount("/restaurants", restaurant.Routes())
-	r.Mount("/auth", user.Routes())
+	r.Mount("/cart", cart.Routes(c.Cart))
+	r.Mount("/menu", menu.Routes(c.Menu))
+	r.Mount("/restaurants", restaurant.Routes(c.Restaurant))
+	r.Mount("/auth", user.Routes(c.User))
+	r.Mount("/order", order.Routes(c.Order))
 	r.Get("/swagger/*", httpSwagger.Handler(httpSwagger.URL("doc.json")))
-	r.Mount("/cart", cart.Routes())
 	return r
 }
