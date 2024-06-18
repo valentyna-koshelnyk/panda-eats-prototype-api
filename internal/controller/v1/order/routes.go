76a59382c54794ae84fb2c1fc455b4fc1208edc2
v.koshelnyk <valentyna.koshelnyk@deliveryhero.com>
2024-06-18 14:05:07 +0200

    #29 feat: add jwtauth middleware for order

--- internal/controller/v1/order/routes.go
+++ internal/controller/v1/order/routes.go
@@ -2,21 +2,27 @@ package order
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/jwtauth/v5"
+	"github.com/spf13/viper"
 )
 
 var tokenAuth *jwtauth.JWTAuth
 
-//func init() {
-//	tokenAuth = jwtauth.New("HS256", []byte(viper.GetString("secret.key")), nil)
-//}
+func init() {
+	tokenAuth = jwtauth.New("HS256", []byte(viper.GetString("secret.key")), nil)
+}
 
 func Routes(c OrderController) chi.Router {
 	r := chi.NewRouter()
-	r.Post("/", c.CreateOrder)
-	r.Patch("/shipped", c.UpdateOrderStatusShipped)
-	r.Patch("/deliver", c.UpdateOrderStatusDelivered)
-	r.Get("/orders", c.GetOrdersHistory)
-	
+	r.Use(middleware.Logger)
+	r.Group(func(r chi.Router) {
+		r.Use(jwtauth.Verifier(tokenAuth))
+		r.Use(jwtauth.Authenticator(tokenAuth))
+		r.Post("/", c.CreateOrder)
+		r.Patch("/shipped", c.UpdateOrderStatusShipped)
+		r.Patch("/deliver", c.UpdateOrderStatusDelivered)
+		r.Get("/orders", c.GetOrdersHistory)
+	})
 	return r
 }
