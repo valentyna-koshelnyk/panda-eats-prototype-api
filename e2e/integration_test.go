be32cb07e6fb952778307bde1105b21e574535d0
v.koshelnyk <valentyna.koshelnyk@deliveryhero.com>
2024-06-27 15:30:47 +0200

    noticket: test: refactor integration test

--- e2e/integration_test.go
+++ e2e/integration_test.go
@@ -1,6 +1,9 @@
-package integration_test
+package e2e
 
 import (
+	"encoding/json"
+	"github.com/tidwall/gjson"
+	"github.com/valentyna-koshelnyk/panda-eats-prototype-api/internal/domain/entity"
 	"io"
 	"net/http"
 	"strings"
@@ -9,306 +12,307 @@ import (
 
 var (
 	userJSON = `{
-    "email": "newUser@example.com",
+    "email": "newUser21@example.com",
     "password": "password1234!"
 }`
 	quantity = `{"quantity" : 3}`
 )
 
-func Test_RestaurantsListing(t *testing.T) {
+func Test_AllEndpoints(t *testing.T) {
 	client := &http.Client{}
-	req, err := http.NewRequest("GET", "http://localhost:3000/api/v1/restaurants?price_range=$&zip_code=23834", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
+	tokenData := &entity.TokenData{}
 
-	resp, err := client.Do(req)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer resp.Body.Close()
+	t.Run("Get All Restaurants", func(t *testing.T) {
+		req, err := http.NewRequest("GET", "http://localhost:3000/api/v1/restaurants?price_range=$&zip_code=23834", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		resp, err := client.Do(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer resp.Body.Close()
 
-	if resp.StatusCode != http.StatusOK {
-		t.Errorf("Expected status code 200, got %d", resp.StatusCode)
-	}
+		if resp.StatusCode != http.StatusOK {
+			t.Errorf("Expected status code 200, got %d", resp.StatusCode)
+		}
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Logf("Received response: %s", body)
+	})
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Logf("Received response: %s", body)
-}
+	t.Run("Get All Menu", func(t *testing.T) {
+		req, err := http.NewRequest("GET", "http://localhost:3000/api/v1/menu/restaurant/3", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-func Test_MenuListing(t *testing.T) {
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", "http://localhost:3000/api/v1/menu/restaurant/3", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
+		resp, err := client.Do(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer resp.Body.Close()
 
-	resp, err := client.Do(req)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer resp.Body.Close()
+		if resp.StatusCode != http.StatusOK {
+			t.Errorf("Expected status code 200, got %d", resp.StatusCode)
+		}
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	if resp.StatusCode != http.StatusOK {
-		t.Errorf("Expected status code 200, got %d", resp.StatusCode)
-	}
+		t.Logf("Received response: %s", body)
+	})
+	t.Run("Registration", func(t *testing.T) {
+		req, err := http.NewRequest("POST", "http://localhost:3000/api/v1/auth/signup", strings.NewReader(userJSON))
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Logf("Received response: %s", body)
-}
+		resp, err := client.Do(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer resp.Body.Close()
 
-func Test_Registration(t *testing.T) {
-	client := &http.Client{}
+		if resp.StatusCode != http.StatusCreated {
+			t.Errorf("Expected status code 200, got %d", resp.StatusCode)
+		}
 
-	req, err := http.NewRequest("POST", "http://localhost:3000/api/v1/auth/signup", strings.NewReader(userJSON))
-	if err != nil {
-		t.Fatal(err)
-	}
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Logf("Received response: %s", body)
+	})
+	t.Run("Login", func(t *testing.T) {
+		req, err := http.NewRequest("POST", "http://localhost:3000/api/v1/auth/login", strings.NewReader(userJSON))
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	resp, err := client.Do(req)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer resp.Body.Close()
+		resp, err := client.Do(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer resp.Body.Close()
 
-	if resp.StatusCode != http.StatusCreated {
-		t.Errorf("Expected status code 200, got %d", resp.StatusCode)
-	}
+		if resp.StatusCode != http.StatusOK {
+			t.Errorf("Expected status code 200, got %d", resp.StatusCode)
+		}
+		var data entity.CustomResponse
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		_ = json.Unmarshal(body, &data)
+		tokenRAW := gjson.Get(string(body), "data.token").String()
+		tokenData.Token = "Bearer " + tokenRAW
+		t.Logf("Received response: %s", tokenData.Token)
+	})
+	t.Run("Add_Item", func(t *testing.T) {
+		req, err := http.NewRequest("POST", "http://localhost:3000/api/v1/cart/item/12", strings.NewReader(quantity))
+		req.Header.Add("Authorization", tokenData.Token)
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Logf("Received response: %s", body)
-}
+		resp, err := client.Do(req)
 
-func Test_Login(t *testing.T) {
-	client := &http.Client{}
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer resp.Body.Close()
 
-	req, err := http.NewRequest("POST", "http://localhost:3000/api/v1/auth/login", strings.NewReader(userJSON))
-	if err != nil {
-		t.Fatal(err)
-	}
+		if resp.StatusCode != http.StatusCreated {
+			t.Errorf("Expected status code 200, got %d", resp.StatusCode)
+		}
 
-	resp, err := client.Do(req)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer resp.Body.Close()
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Logf("Received response: %s", body)
+	})
+	t.Run("Add_Item", func(t *testing.T) {
+		req, err := http.NewRequest("POST", "http://localhost:3000/api/v1/cart/item/43", strings.NewReader(quantity))
+		req.Header.Add("Authorization", tokenData.Token)
 
-	if resp.StatusCode != http.StatusOK {
-		t.Errorf("Expected status code 200, got %d", resp.StatusCode)
-	}
+		resp, err := client.Do(req)
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Logf("Received response: %s", body)
-}
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer resp.Body.Close()
 
-func Test_AddItem(t *testing.T) {
-	client := &http.Client{}
-	req, err := http.NewRequest("POST", "http://localhost:3000/api/v1/cart/item/12", nil)
-	req.Header.Add("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMjc3ZmZhMmMtODYzMC00YjBkLWFmMzQtNzhlYzhlN2FmMmY3IiwiZXhwIjoxNzE5MzM4MTQyfQ.X6jJ0D7mlsiEh29XnNBAG6YG4QN6FJrkZ_P2r4pEFEo")
-	if err != nil {
-		t.Fatal(err)
-	}
+		if resp.StatusCode != http.StatusCreated {
+			t.Errorf("Expected status code 200, got %d", resp.StatusCode)
+		}
 
-	resp, err := client.Do(req)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer resp.Body.Close()
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Logf("Received response: %s", body)
+	})
+	t.Run("Get cart items", func(t *testing.T) {
+		req, err := http.NewRequest("GET", "http://localhost:3000/api/v1/cart", nil)
+		req.Header.Add("Authorization", tokenData.Token)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	if resp.StatusCode != http.StatusCreated {
-		t.Errorf("Expected status code 200, got %d", resp.StatusCode)
-	}
+		resp, err := client.Do(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer resp.Body.Close()
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Logf("Received response: %s", body)
-}
+		if resp.StatusCode != http.StatusOK {
+			t.Errorf("Expected status code 200, got %d", resp.StatusCode)
+		}
 
-func Test_GetCartItem(t *testing.T) {
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", "http://localhost:3000/api/v1/cart", nil)
-	req.Header.Add("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMjc3ZmZhMmMtODYzMC00YjBkLWFmMzQtNzhlYzhlN2FmMmY3IiwiZXhwIjoxNzE5MzM4MTQyfQ.X6jJ0D7mlsiEh29XnNBAG6YG4QN6FJrkZ_P2r4pEFEo")
-	if err != nil {
-		t.Fatal(err)
-	}
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Logf("Received response: %s", body)
+	})
+	t.Run("Remove items", func(t *testing.T) {
+		req, err := http.NewRequest("DELETE", "http://localhost:3000/api/v1/cart/item/43", nil)
+		req.Header.Add("Authorization", tokenData.Token)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	resp, err := client.Do(req)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer resp.Body.Close()
+		resp, err := client.Do(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer resp.Body.Close()
 
-	if resp.StatusCode != http.StatusOK {
-		t.Errorf("Expected status code 200, got %d", resp.StatusCode)
-	}
+		if resp.StatusCode != http.StatusOK {
+			t.Errorf("Expected status code 200, got %d", resp.StatusCode)
+		}
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Logf("Received response: %s", body)
-}
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Logf("Received response: %s", body)
+	})
+	t.Run("Update item quantity", func(t *testing.T) {
+		req, err := http.NewRequest("PATCH", "http://localhost:3000/api/v1/cart/item/12", strings.NewReader(quantity))
+		req.Header.Add("Authorization", tokenData.Token)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-func Test_RemoveItems(t *testing.T) {
-	client := &http.Client{}
-	req, err := http.NewRequest("DELETE", "http://localhost:3000/api/v1/cart/item/12", nil)
-	req.Header.Add("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMjc3ZmZhMmMtODYzMC00YjBkLWFmMzQtNzhlYzhlN2FmMmY3IiwiZXhwIjoxNzE5MzM4MTQyfQ.X6jJ0D7mlsiEh29XnNBAG6YG4QN6FJrkZ_P2r4pEFEo")
-	if err != nil {
-		t.Fatal(err)
-	}
+		resp, err := client.Do(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer resp.Body.Close()
 
-	resp, err := client.Do(req)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer resp.Body.Close()
+		if resp.StatusCode != http.StatusCreated {
+			t.Errorf("Expected status code 200, got %d", resp.StatusCode)
+		}
 
-	if resp.StatusCode != http.StatusOK {
-		t.Errorf("Expected status code 200, got %d", resp.StatusCode)
-	}
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Logf("Received response: %s", body)
+	})
+	t.Run("Create order", func(t *testing.T) {
+		req, err := http.NewRequest("POST", "http://localhost:3000/api/v1/order", nil)
+		req.Header.Add("Authorization", tokenData.Token)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Logf("Received response: %s", body)
-}
+		resp, err := client.Do(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer resp.Body.Close()
 
-func Test_UpdateItemQuantity(t *testing.T) {
-	client := &http.Client{}
-	req, err := http.NewRequest("PATCH", "http://localhost:3000/api/v1/cart/item/12", strings.NewReader(quantity))
-	req.Header.Add("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMjc3ZmZhMmMtODYzMC00YjBkLWFmMzQtNzhlYzhlN2FmMmY3IiwiZXhwIjoxNzE5MzM4MTQyfQ.X6jJ0D7mlsiEh29XnNBAG6YG4QN6FJrkZ_P2r4pEFEo")
-	if err != nil {
-		t.Fatal(err)
-	}
+		if resp.StatusCode != http.StatusCreated {
+			t.Errorf("Expected status code 200, got %d", resp.StatusCode)
+		}
 
-	resp, err := client.Do(req)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer resp.Body.Close()
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Logf("Received response: %s", body)
+	})
+	t.Run("Ship order", func(t *testing.T) {
+		req, err := http.NewRequest("PATCH", "http://localhost:3000/api/v1/order/pTSZIngsJbN0pGmlYFtyt/shipping", nil)
+		req.Header.Add("Authorization", tokenData.Token)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	if resp.StatusCode != http.StatusCreated {
-		t.Errorf("Expected status code 200, got %d", resp.StatusCode)
-	}
+		resp, err := client.Do(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer resp.Body.Close()
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Logf("Received response: %s", body)
-}
+		if resp.StatusCode != http.StatusAccepted {
+			t.Errorf("Expected status code 200, got %d", resp.StatusCode)
+		}
 
-func Test_CreateOrder(t *testing.T) {
-	client := &http.Client{}
-	req, err := http.NewRequest("POST", "http://localhost:3000/api/v1/order", nil)
-	req.Header.Add("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMjc3ZmZhMmMtODYzMC00YjBkLWFmMzQtNzhlYzhlN2FmMmY3IiwiZXhwIjoxNzE5MzM4MTQyfQ.X6jJ0D7mlsiEh29XnNBAG6YG4QN6FJrkZ_P2r4pEFEo")
-	if err != nil {
-		t.Fatal(err)
-	}
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Logf("Received response: %s", body)
+	})
+	t.Run("Delivery Order", func(t *testing.T) {
+		req, err := http.NewRequest("PATCH", "http://localhost:3000/api/v1/order/pTSZIngsJbN0pGmlYFtyt/delivery", nil)
+		req.Header.Add("Authorization", tokenData.Token)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	resp, err := client.Do(req)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer resp.Body.Close()
+		resp, err := client.Do(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer resp.Body.Close()
 
-	if resp.StatusCode != http.StatusCreated {
-		t.Errorf("Expected status code 200, got %d", resp.StatusCode)
-	}
+		if resp.StatusCode != http.StatusAccepted {
+			t.Errorf("Expected status code 200, got %d", resp.StatusCode)
+		}
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Logf("Received response: %s", body)
-}
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Logf("Received response: %s", body)
+	})
+	t.Run("Get Order History", func(t *testing.T) {
+		req, err := http.NewRequest("GET", "http://localhost:3000/api/v1/order/history", nil)
+		req.Header.Add("Authorization", tokenData.Token)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-func Test_ShipOrder(t *testing.T) {
-	client := &http.Client{}
-	req, err := http.NewRequest("PATCH", "http://localhost:3000/api/v1/order/b7bJlJLRMGpyoC3LXbaSt/shipping", nil)
-	req.Header.Add("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMjc3ZmZhMmMtODYzMC00YjBkLWFmMzQtNzhlYzhlN2FmMmY3IiwiZXhwIjoxNzE5MzM4MTQyfQ.X6jJ0D7mlsiEh29XnNBAG6YG4QN6FJrkZ_P2r4pEFEo")
-	if err != nil {
-		t.Fatal(err)
-	}
+		resp, err := client.Do(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer resp.Body.Close()
 
-	resp, err := client.Do(req)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer resp.Body.Close()
+		if resp.StatusCode != http.StatusOK {
+			t.Errorf("Expected status code 200, got %d", resp.StatusCode)
+		}
 
-	if resp.StatusCode != http.StatusCreated {
-		t.Errorf("Expected status code 200, got %d", resp.StatusCode)
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Logf("Received response: %s", body)
-}
-
-func Test_DeliverOrder(t *testing.T) {
-	client := &http.Client{}
-	req, err := http.NewRequest("PATCH", "http://localhost:3000/api/v1/order/b7bJlJLRMGpyoC3LXbaSt/delivery", nil)
-	req.Header.Add("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMjc3ZmZhMmMtODYzMC00YjBkLWFmMzQtNzhlYzhlN2FmMmY3IiwiZXhwIjoxNzE5MzM4MTQyfQ.X6jJ0D7mlsiEh29XnNBAG6YG4QN6FJrkZ_P2r4pEFEo")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusCreated {
-		t.Errorf("Expected status code 200, got %d", resp.StatusCode)
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Logf("Received response: %s", body)
-}
-
-func Test_GetOrderHistory(t *testing.T) {
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", "http://localhost:3000/api/v1/order/history", nil)
-	req.Header.Add("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMjc3ZmZhMmMtODYzMC00YjBkLWFmMzQtNzhlYzhlN2FmMmY3IiwiZXhwIjoxNzE5MzM4MTQyfQ.X6jJ0D7mlsiEh29XnNBAG6YG4QN6FJrkZ_P2r4pEFEo")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		t.Errorf("Expected status code 200, got %d", resp.StatusCode)
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Logf("Received response: %s", body)
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Logf("Received response: %s", body)
+	})
 }
