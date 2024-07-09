ea9e643270cb8284c6c499f04f959fe88d74eb43
beethagoras <50967267+valentyna-koshelnyk@users.noreply.github.com>
2024-07-09 09:38:24 +0200

    Update main.tf

--- testenv/main.tf
+++ testenv/main.tf
@@ -6,7 +6,6 @@ provider "aws" {
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
-  s3_use_path_style           = true
   endpoints {
     dynamodb = "http://localhost:4566"
     s3 = "http://localhost:4566"
